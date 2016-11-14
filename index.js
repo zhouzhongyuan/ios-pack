ce554c53ccf8c05fbd68dd374acf4fe91598a4e8
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-11-14 15:01:00 +0800

    add route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
+var pack = require('./route/pack');
+
+app.use('/pack', pack);
 
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-
 var server = app.listen(4000, function () {
     var host = server.address().address;
     var port = server.address().port;
--- pack.js
+++ pack.js
@@ -1,46 +1,37 @@
 const process = require('process');
 const spawn = require('child_process').spawn;
-const userHome = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
-const projectPath = 'project/YIGO_IOS_PACK-del/yes_ios_0_0';
-const workspacePath = 'project/YIGO_IOS_PACK-del/yes_ios_0_0/project/workspace';
+const fs = require('fs-extra');
+var Client = require('svn-spawn');
+
+//生成ipa的配置文件
 const exportOptions = [__dirname,'exportOptions.plist'].join('/');
-
-
+// 系统home文件夹
+const userHome = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+// workspace
+const workspacePath = 'project/YIGO_IOS_PACK-del/yes_ios_0_0/project/workspace';
 const workspaceFullPath = [userHome, workspacePath].join('/');
-
-
-
-
+// svn
+const projectPath = 'project/YIGO_IOS_PACK-del/yes_ios_0_0';
+const projectFullPath = [userHome, projectPath].join('/');
+// destination
 // TODO fake
 const releasePath = 'project/ios_compile/pack2/yigomobile/public/release/test';
 // const releasePath = 'project/ios_compile/pack2/yigomobile/public/release/com.bokesoft.yigomobile/ios';
 const releaseFullPath = [userHome, releasePath, '/yesapp.ipa'].join('/');
 
-const fs = require('fs-extra')
 
 
 
 
 // 1.svn update
-const projectFullPath = [userHome, projectPath].join('/');
 process.chdir(projectFullPath);
-var Client = require('svn-spawn');
 var client = new Client({
-    username: 'zhouzy', // optional if authentication not required or is already saved
-    password: 'zhouzy', // optional if authentication not required or is already saved
-    noAuthCache: true, // optional, if true, username does not become the logged in user on the machine
+    username: 'zhouzy',
+    password: 'zhouzy',
+    noAuthCache: true,
 });
 
-function updateSvn() {
-    return new Promise(function (resolve, reject) {
-        client.update(function(err, data) {
-            if(err){
-                reject(new Error(err))
-            }
-            resolve(data);
-        });
-    });
-};
+
 updateSvn()
     .then(function () {
         process.chdir(workspaceFullPath);
@@ -55,13 +46,22 @@ updateSvn()
     })
     .then(function () {
         return release();
-
     })
     .catch(function (e) {
         console.log('ERROR');
         console.log(e)
     });
-
+// svn update
+function updateSvn() {
+    return new Promise(function (resolve, reject) {
+        client.update(function(err, data) {
+            if(err){
+                reject(new Error(err))
+            }
+            resolve(data);
+        });
+    });
+};
 // archive
 function archive() {
     return new Promise(function (resolve, reject) {
@@ -77,7 +77,6 @@ function archive() {
         });
     })
 }
-
 // 生成ipa
 function ipa() {
     return new Promise(function (resolve, reject) {
@@ -90,7 +89,6 @@ function ipa() {
             if(!data.match(/_createLoggingBundleAtPath/) && !data.match(/1./) && !data.match(/61/) ){
                 reject(data);
             }
-
         });
         ls.on('close', (code) => {
             resolve(code);

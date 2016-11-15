daa3890d882054ca484c1c472d805a58f88844cc
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2016-11-15 10:37:41 +0800

    调整整个项目的结构

---
+++ routes/pack/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var pack = require('./pack');
+
+var status = {
+  busy: false
+};
+
+router.get('/', function(req, res, next) {
+    res.render('pack', { title: 'Express' });
+    // res.sendFile(__dirname, '../public/index.html');
+});
+router.get('/status', function(req, res, next) {
+  res.json(status);
+});
+router.post('/', function(req, res, next) {
+  status.busy = true;
+  (function () {
+    pack()
+        .then(function () {
+          status.busy = false;
+          res.send('打包成功');
+        })
+        .catch(function () {
+          status.busy = false;
+          res.send('打包失败');
+        });
+    res.send('打包中');
+  })();
+
+});
+
+module.exports = router;

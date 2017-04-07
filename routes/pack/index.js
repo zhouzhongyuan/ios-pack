abf0fb79baa275c399b85366cfdeb2688b83f5e2
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-04-07 11:01:37 +0800

    Lint file

--- routes/pack/index.js
+++ routes/pack/index.js
@@ -1,36 +1,31 @@
-var express = require('express');
-var router = express.Router();
-var pack = require('./pack');
-var plist = require('../plist');
-
-var status = {
-  busy: false
+const express = require('express');
+const router = express.Router();
+const pack = require('./pack');
+const plist = require('../plist');
+const status = {
+    busy: false,
 };
-
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
     res.render('pack', { title: '打包' });
 });
-router.get('/status', function(req, res, next) {
-  res.json(status);
+router.get('/status', (req, res, next) => {
+    res.json(status);
 });
-router.post('/', function(req, res, next) {
-  status.busy = true;
-  (function () {
-    pack()
-        .then(
-
-        )
-        .then(function () {
-          status.busy = false;
-          res.send('打包成功');
-        })
-        .catch(function () {
-          status.busy = false;
-          res.send('打包失败');
-        });
-    res.send('打包中');
-  })();
-
+router.post('/', (req, res, next) => {
+    status.busy = true;
+    (function () {
+        pack()
+            .then(
+            )
+            .then(() => {
+                status.busy = false;
+                res.send('打包成功');
+            })
+            .catch(() => {
+                status.busy = false;
+                res.send('打包失败');
+            });
+        res.send('打包中');
+    }());
 });
-
 module.exports = router;

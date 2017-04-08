5c8baf3d359e2046013a55b9614fff4b303390cc
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-04-08 14:49:52 +0800

    Add mongo database

--- models/task.js
+++ models/task.js
@@ -3,11 +3,7 @@ const taskSchema = new mongoose.Schema({
     svnUrl: String,
     svnUsername: String,
     svnPassword: String,
+    // projectId: String, //TODO 不应该保存前三个，而应该使用projectId
     version: String,
-    user: String,
-    password: String,
-    packageName: String,
-    icon: String,
-    plugin: Array,
 });
 module.exports = mongoose.model('Task', taskSchema);

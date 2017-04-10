564088ab27df0b6ebd69cb9fdc793c5f393e9a1e
zhouzhongyuan <yawenzhongyuan@gmail.com>
2017-04-10 14:36:11 +0800

    Update schema

--- models/task.js
+++ models/task.js
@@ -1,9 +1,18 @@
 import mongoose from 'mongoose';
-import { projectSchema } from './task';
+import { projectSchema } from './project';
 const taskSchema = new mongoose.Schema({
+    id: String,
     projectId: String,
-    version: String,
     project: projectSchema,
+    version: String,
+    platform: String,
+    configuration: String,
+    release: Boolean,
+    status: {
+        code: String,
+        log: String,
+    },
+    dateOfCreate: { type: Date, default: Date.now },
 });
 const Task = mongoose.model('Task', taskSchema);
 export default Task;

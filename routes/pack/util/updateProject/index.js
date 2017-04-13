04951e273919c77b612eb0b24f9fec343f9a4319
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2017-04-13 14:22:00 +0800

    Lint

--- routes/pack/util/updateProject/index.js
+++ routes/pack/util/updateProject/index.js
@@ -2,6 +2,7 @@ import { Project, Task } from '../../../../models/index';
 async function updateProject(id,obj){
     const project = await Project.findById(id).exec();
     Object.assign(project,obj);
+    console.log(project)
     await project.save();
 }
 export default updateProject;

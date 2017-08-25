19101bb60f640de5a439ec348cdb2b812e51e565
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2017-08-25 11:04:00 +0800

    Update download function

--- pack/util/download/index.js
+++ pack/util/download/index.js
@@ -2,9 +2,15 @@ import request from 'request';
 import fs from 'fs';
 function download(url, file) {
     return new Promise((resolve, reject) => {
-        const stream = request(url).pipe(fs.createWriteStream(file));
+        const stream =
+            request
+                .get(url)
+                .on('error', (err) => {
+                    reject(`Download file '${url}' error, ${err.message}`);
+                })
+                .pipe(fs.createWriteStream(file));
         stream.on('error', (err) => {
-            reject(err);
+            reject(`Stream error, ${err.message}`);
         });
         stream.on('finish', () => {
             resolve('Download success');

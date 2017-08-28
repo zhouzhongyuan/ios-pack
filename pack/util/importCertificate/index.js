a456e8ff193adc8c012ba98631df8deea85b9945
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2017-08-28 11:03:12 +0800

    Fix not throwing error in import-certificate error.

--- pack/util/importCertificate/index.js
+++ pack/util/importCertificate/index.js
@@ -1,5 +1,4 @@
 import { default as ic } from 'import-certificate';
-import fs from 'fs-extra-promise';
 import download from '../download/';
 async function importCertificate(cerUrl, cerPassword) {
     const file = 'cer.p12';
@@ -7,6 +6,8 @@ async function importCertificate(cerUrl, cerPassword) {
         await download(cerUrl, file);
         const value = await ic(file, cerPassword);
         return value;
+    } catch (e) {
+        throw (e);
     } finally {
         await fs.remove(file);
     }

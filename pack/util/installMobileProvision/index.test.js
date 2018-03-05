2d6a995dca55ebdc2e9b265525d88c558643585b
Zhou Zhongyuan <yawenzhongyuan@gmail.com>
2018-03-05 11:05:59 +0800

    Update mobileprovision

--- pack/util/installMobileProvision/index.test.js
+++ pack/util/installMobileProvision/index.test.js
@@ -1,9 +1,7 @@
 import imp from './index';
-// const url = 'http://1.1.8.34:3001/download?id=58ee3b9c18679c2c5dc0f9d6';
 const url = 'https://dev.bokesoft.com/yigomobile2/download?id=59a4c099f094c527bc0fe9a2'; // thgn
 test('get right arguments from mobileprovision file', async () => {
     expect.assertions(1);
     const d = await imp(url);
-    console.log(d);
-    expect(d).toBe('hi');
+    expect(d.data.Name).toBe('com.trinasolar.mobile');
 });

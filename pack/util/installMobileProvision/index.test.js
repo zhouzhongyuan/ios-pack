7afc96b0994864d6ccb7e52a55b441cc17a03641
Zhongyuan <yawenzhongyuan@gmail.com>
2018-03-05 09:49:33 +0800

    Add jest

--- pack/util/installMobileProvision/index.test.js
+++ pack/util/installMobileProvision/index.test.js
@@ -1,8 +1,9 @@
 import imp from './index';
 // const url = 'http://1.1.8.34:3001/download?id=58ee3b9c18679c2c5dc0f9d6';
-const url = 'https://dev.bokesoft.com/yigomobile/public/release/cn.com.del/zjpl.mobileprovision';
-imp(url).then((d) => {
+const url = 'https://dev.bokesoft.com/yigomobile2/download?id=59a4c099f094c527bc0fe9a2'; // thgn
+test('get right arguments from mobileprovision file', async () => {
+    expect.assertions(1);
+    const d = await imp(url);
     console.log(d);
-}).catch((err) => {
-    console.log(err);
+    expect(d).toBe('hi');
 });
